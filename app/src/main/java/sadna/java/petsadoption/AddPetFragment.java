4c2c417ba1fe70fdf859ee28aa3ad7132f536b2d
Talgoo <30302888+Khatulal@users.noreply.github.com>
2022-03-07 17:30:53 +0200

    Added Add pet fragment

--- app/src/main/java/sadna/java/petsadoption/AddPetFragment.java
+++ app/src/main/java/sadna/java/petsadoption/AddPetFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,14 +21,14 @@ public class AddPetFragment extends Fragment {
     private ImageView image;
     private Spinner spGenus;
 
-
+    private Pet newPet;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentAddPetBinding.inflate(inflater, container, false);
-        spGenus = binding.sGenusContentAdd;
+        spGenus = binding.spGenusContentAdd;
         spGenus.setSelection(1);
 
         return inflater.inflate(R.layout.fragment_add_pet, container, false);
@@ -36,6 +37,28 @@ public class AddPetFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        binding.btnAddPet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int Genus = binding.spGenusContentAdd.getSelectedItemPosition();
+                String Name = binding.etPetNameContentAdd.getText().toString();
+                Integer Identifier;
+                String Breed = binding.etBreedContentAdd.getText().toString();
+                int Sex = binding.spSexContentAdd.getSelectedItemPosition();
+                Integer Age = Integer.parseInt(binding.etAgeContentAdd.getText().toString());
+                Integer Weight = Integer.parseInt(binding.etWeightContentAdd.getText().toString());
+                Boolean Vaccinated = binding.cbVaccinatedAdd.isChecked();
+                int Diet = binding.spDietContentAdd.getSelectedItemPosition();
+                String Description = binding.etDescriptionContentAdd.getText().toString();
+            }
+        });
+
+        binding.btnBackAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     @Override
